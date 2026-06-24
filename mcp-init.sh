89d8a0aa300f1c52b3847c5e6e5b59d7563fb172
nie.chao <nie.chao@advantech.com.cn>
2026-06-24 15:41:06 +0800

    fix init shell

--- mcp-init.sh
+++ mcp-init.sh
@@ -130,8 +130,10 @@ ${DEVOPS_USER} ALL=(ALL) NOPASSWD: /bin/systemctl status *
 # 日志查看
 ${DEVOPS_USER} ALL=(ALL) NOPASSWD: /usr/bin/journalctl
 
-# 目录创建（支持在任意位置创建工作目录）
-${DEVOPS_USER} ALL=(ALL) NOPASSWD: /usr/bin/mkdir
+# 基础文件与权限初始化工具
+${DEVOPS_USER} ALL=(ALL) NOPASSWD: /usr/bin/mkdir *
+${DEVOPS_USER} ALL=(ALL) NOPASSWD: /usr/bin/chown *
+${DEVOPS_USER} ALL=(ALL) NOPASSWD: /usr/bin/chmod *
 
 # 如命令种类繁多，可暂时放开全部权限（安全性降低）
 # ${DEVOPS_USER} ALL=(ALL) NOPASSWD: ALL
